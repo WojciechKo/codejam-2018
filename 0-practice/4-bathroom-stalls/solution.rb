6a0ec344389a409d41e75b4b658b2afc779c9f5c
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2018-04-01 13:41:37 +0100

    Math to the rescue

--- 0-practice/4-bathroom-stalls/solution.rb
+++ 0-practice/4-bathroom-stalls/solution.rb
@@ -4,62 +4,12 @@ require 'logger'
 
 log_file = File.open('log.log', 'a')
 LOGGER = Logger.new(log_file)
-LOGGER.level = Logger::WARN
-
-class Stall < Struct.new(:occupied, :index, :left, :right)
-  def initialize(*args)
-    super
-    @dirty = true
-  end
-
-  def left=(left)
-    super
-    @dirty = true
-  end
-
-  def right=(right)
-    super
-    @dirty = true
-  end
-
-  def occupied=(occupied)
-    super
-    @dirty = true
-  end
-
-  def <=>(other_stall)
-    value <=> other_stall.value
-  end
-
-  def value
-    if @dirty
-      @dirty = false
-      @value = calculate_value
-    else
-      @value
-    end
-  end
-
-  def minmax
-    [left, right].minmax
-  end
-
-  private
-
-  def calculate_value
-    return [0, 0] if occupied
-
-    minmax + [-index]
-  end
-end
+LOGGER.level = Logger::DEBUG
 
 class StallPicker
   def initialize(stalls_count)
-    @stalls = Array.new(stalls_count) do |number|
-      Stall.new(false, number, number, stalls_count - number - 1)
-    end
-    @available_stalls = @stalls.clone
-    LOGGER.debug("## Init stalls: #{@stalls}")
+    @spaces = [stalls_count]
+    LOGGER.debug("## Init spaces: #{@spaces}")
   end
 
   def pick_multiple(n)
@@ -67,33 +17,17 @@ class StallPicker
   end
 
   def pick
-    @available_stalls.max.tap do |selected_stall|
-      LOGGER.debug("#### Selected: #{selected_stall}")
-      make_stall_occupied!(selected_stall.index)
-    end
-  end
+    biggest_space = @spaces.max
+    biggest_index = @spaces.index(biggest_space)
+    biggest_space = @spaces.delete_at(biggest_index)
 
-  private
-  attr_reader :stalls, :available_stalls
-
-  def make_stall_occupied!(selected_stall)
-    @stalls[selected_stall].occupied = true
-
-    @stalls.take(selected_stall).reverse.each_with_index do |stall, index|
-      stall.right = index
-      LOGGER.debug("#### Update on left side: #{stall}")
-      break if stall.occupied
-    end
-
-    @stalls.drop(selected_stall + 1).each_with_index do |stall, index|
-      stall.left = index
-      LOGGER.debug("#### Update on right side: #{stall}")
-      break if stall.occupied
-    end
-
-    @available_stalls.delete_if { |stall| stall.index == selected_stall }
-
-    LOGGER.debug("## After update: #{@stalls}")
+    half = biggest_space / 2
+    div_rest = biggest_space % 2
+    left = half + div_rest - 1
+    right = half
+    @spaces << left unless left == 0
+    @spaces << right unless right == 0
+    [left, right]
   end
 end
 
@@ -105,8 +39,7 @@ class Interface
         LOGGER.debug("Stalls : #{stalls}")
         LOGGER.debug("People : #{people}")
 
-        stall = StallPicker.new(stalls).pick_multiple(people)
-        min, max = stall.minmax
+        min, max = StallPicker.new(stalls).pick_multiple(people)
         output_log = "Case ##{test_case + 1}: #{max} #{min}"
         puts output_log
         LOGGER.debug(output_log)
--- 0-practice/4-bathroom-stalls/spec/picker_spec.rb
+++ 0-practice/4-bathroom-stalls/spec/picker_spec.rb
@@ -2,41 +2,55 @@ require 'stringio'
 require_relative '../solution'
 
 RSpec.describe StallPicker do
-  it 'works for 4 stalls' do
-    picker = StallPicker.new(4)
-    expect(picker.pick.index).to eq(1)
-    expect(picker.pick.index).to eq(2)
-    expect(picker.pick.index).to eq(0)
-    expect(picker.pick.index).to eq(3)
-  end
+  describe '#pick' do
+    it 'works for 4 stallsf' do
+      picker = StallPicker.new(4)
+      expect(picker.pick).to eq([1, 2])
+      expect(picker.pick).to eq([0, 1])
+      expect(picker.pick).to eq([0, 0])
+      expect(picker.pick).to eq([0, 0])
+    end
 
-  it 'works for 5 stalls' do
-    picker = StallPicker.new(5)
-    expect(picker.pick.index).to eq(2)
-    expect(picker.pick.index).to eq(0)
-    expect(picker.pick.index).to eq(3)
-    expect(picker.pick.index).to eq(1)
-    expect(picker.pick.index).to eq(4)
-  end
+    it 'works for 5 stallsf' do
+      picker = StallPicker.new(5)
+      expect(picker.pick).to eq([2, 2])
+      expect(picker.pick).to eq([0, 1])
+      expect(picker.pick).to eq([0, 1])
+      expect(picker.pick).to eq([0, 0])
+      expect(picker.pick).to eq([0, 0])
+    end
 
-  it 'works for 6 stalls' do
-    picker = StallPicker.new(6)
-    expect(picker.pick.index).to eq(2)
-    expect(picker.pick.index).to eq(4)
-    expect(picker.pick.index).to eq(0)
-    expect(picker.pick.index).to eq(1)
-    expect(picker.pick.index).to eq(3)
-    expect(picker.pick.index).to eq(5)
-  end
+    it 'works for 6 stallsf' do
+      picker = StallPicker.new(6)
+      expect(picker.pick).to eq([2, 3])
+      expect(picker.pick).to eq([1, 1])
+      expect(picker.pick).to eq([0, 1])
+      expect(picker.pick).to eq([0, 0])
+      expect(picker.pick).to eq([0, 0])
+      expect(picker.pick).to eq([0, 0])
+    end
 
-  it 'works for 7 stalls' do
-    picker = StallPicker.new(7)
-    expect(picker.pick.index).to eq(3)
-    expect(picker.pick.index).to eq(1)
-    expect(picker.pick.index).to eq(5)
-    expect(picker.pick.index).to eq(0)
-    expect(picker.pick.index).to eq(2)
-    expect(picker.pick.index).to eq(4)
-    expect(picker.pick.index).to eq(6)
+    it 'works for 7 stallsf' do
+      picker = StallPicker.new(7)
+      expect(picker.pick).to eq([3, 3])
+      expect(picker.pick).to eq([1, 1])
+      expect(picker.pick).to eq([1, 1])
+      expect(picker.pick).to eq([0, 0])
+      expect(picker.pick).to eq([0, 0])
+      expect(picker.pick).to eq([0, 0])
+      expect(picker.pick).to eq([0, 0])
+    end
+
+    it 'works for 8 stallsf' do
+      picker = StallPicker.new(8)
+      expect(picker.pick).to eq([3, 4])
+      expect(picker.pick).to eq([1, 2])
+      expect(picker.pick).to eq([1, 1])
+      expect(picker.pick).to eq([0, 1])
+      expect(picker.pick).to eq([0, 0])
+      expect(picker.pick).to eq([0, 0])
+      expect(picker.pick).to eq([0, 0])
+      expect(picker.pick).to eq([0, 0])
+    end
   end
 end
